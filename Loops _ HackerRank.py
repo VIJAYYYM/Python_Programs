f6ecc9e7f55c62625519b82a0de6fd23d4ac3370
VIJAY M <129942667+VIJAYYYM@users.noreply.github.com>
2023-08-03 21:46:01 +0530

    Add files via upload

---
+++ Loops _ HackerRank.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Integer division
+    integer_division = a // b
+    
+    # Float division
+    float_division = a / b
+    
+    print(integer_division)
+    print(float_division)
